ba99f5a8d1f6c40d8d1548727cfdc9913b0ac223
ElvireDev <39312036+elviredev@users.noreply.github.com>
2019-09-24 13:32:31 +0200

    [DAO] Repo Java AppUserRepository & AppRoleRepository

---
+++ src/main/java/intra/poleemploi/dao/AppRoleRepository.java
@@ -0,0 +1,10 @@
+package intra.poleemploi.dao;
+
+import intra.poleemploi.entities.AppRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface AppRoleRepository extends JpaRepository<AppRole, Long> {
+    public AppRole findRoleByRoleName(String roleName);
+}
---
+++ src/main/java/intra/poleemploi/dao/AppUserRepository.java
@@ -0,0 +1,10 @@
+package intra.poleemploi.dao;
+
+import intra.poleemploi.entities.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+    public AppUser findUserByUsername(String username);
+}
