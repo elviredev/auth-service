8318594e38715a74f95b31496c448722404450ab
ElvireDev <39312036+elviredev@users.noreply.github.com>
2019-09-24 13:11:24 +0200

    Objets Métier Java Entity
    AppRole & AppUser

---
+++ src/main/java/intra/poleemploi/entities/AppUser.java
@@ -0,0 +1,24 @@
+package intra.poleemploi.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor @ToString
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private boolean activated;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Collection<AppRole> roles = new ArrayList<>();
+}
