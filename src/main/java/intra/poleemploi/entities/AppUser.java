83d3c573987778079aa1cf0625e63f65a92801bd
ElvireDev <39312036+elviredev@users.noreply.github.com>
2019-09-27 09:59:07 +0200

    Update AppUser.java
    add annotation @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)

--- src/main/java/intra/poleemploi/entities/AppUser.java
+++ src/main/java/intra/poleemploi/entities/AppUser.java
@@ -1,5 +1,6 @@
 package intra.poleemploi.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,6 +20,7 @@ public class AppUser {
     private Long id;
     @Column(unique = true)
     private String username;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private boolean activated;
     @ManyToMany(fetch = FetchType.EAGER)
