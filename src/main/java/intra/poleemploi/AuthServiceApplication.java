5f3291ed0afaa4ce53122ed93a1d9e277764dbdf
ElvireDev <39312036+elviredev@users.noreply.github.com>
2019-10-07 16:00:10 +0200

    Update AuthServiceApplication.java

--- src/main/java/intra/poleemploi/AuthServiceApplication.java
+++ src/main/java/intra/poleemploi/AuthServiceApplication.java
@@ -1,6 +1,5 @@
 package intra.poleemploi;
 
-import intra.poleemploi.entities.AppProduct;
 import intra.poleemploi.entities.AppRole;
 import intra.poleemploi.service.AccountService;
 import org.springframework.boot.CommandLineRunner;
@@ -22,28 +21,18 @@ public class AuthServiceApplication {
 	CommandLineRunner start(AccountService accountService){
 		return args -> {
 			// ajout de 2 roles
-			accountService.saveRole(new AppRole(null, "USER"));
-			accountService.saveRole(new AppRole(null, "ADMIN"));
+			accountService.saveAppRole(new AppRole(null, "USER"));
+			accountService.saveAppRole(new AppRole(null, "ADMIN"));
 
 			// ajout users
 			Stream.of("user1", "user2", "user3", "admin").forEach(username -> {
-				accountService.saveUser(username, "1234", "1234");
-			});
-
-			// ajout products
-			Stream.of("Profil de compétences", "MRS Digitale", "MAP Vue DE", "BEN Pe.fr", "AUDE Presta").forEach(productName -> {
-				accountService.saveProduct(new AppProduct(null, productName));
+				accountService.saveAppUser(username, "1234", "1234");
 			});
 
 			// ajout role ADMIN a l'admin
 			accountService.addRoleToUser("admin", "ADMIN");
 
-			// ajout products à user1
-			accountService.addProductToUser("user1", "Profil de compétences");
-			accountService.addProductToUser("user1", "MAP Vue DE");
 
-			// ajout products à user2
-			accountService.addProductToUser("user2", "MRS Digitale");
 		};
 	}
 
