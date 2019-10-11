7df953e567f8019718a3b142df30988bf19ec6ad
ElvireDev <39312036+elviredev@users.noreply.github.com>
2019-10-11 12:24:37 +0200

    Update UserController.java

--- src/main/java/intra/poleemploi/web/UserController.java
+++ src/main/java/intra/poleemploi/web/UserController.java
@@ -1,21 +1,31 @@
 package intra.poleemploi.web;
 
+import intra.poleemploi.dao.AppUserRepository;
 import intra.poleemploi.entities.AppUser;
 import intra.poleemploi.service.AccountService;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
     @Autowired
     private AccountService accountService;
+    @Autowired
+    private AppUserRepository appUserRepository;
+
     @PostMapping("/register")
     public AppUser register(@RequestBody UserForm userForm){ // données envoyées au format JSON
         return accountService.saveAppUser(userForm.getUsername(), userForm.getPassword(), userForm.getConfirmedPassword());
     }
+
+    @GetMapping(value = "/profile/{username}")
+    public AppUser appUserByUsername(@PathVariable(name="username") String username){
+        return appUserRepository.findUserByUsername(username);
+    };
+
+
 }
 @Data
 class UserForm {
