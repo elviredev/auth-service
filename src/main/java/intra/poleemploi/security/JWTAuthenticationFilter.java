1bdf1ef4711b303060d9be40f6762f30b1fc2a41
ElvireDev <39312036+elviredev@users.noreply.github.com>
2019-10-14 17:39:38 +0200

    Update JWTAuthenticationFilter.java

--- src/main/java/intra/poleemploi/security/JWTAuthenticationFilter.java
+++ src/main/java/intra/poleemploi/security/JWTAuthenticationFilter.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import javax.servlet.Filter;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
